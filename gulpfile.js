bed38cebc7dfc93ac466cae00d407edcfb7572b8
atid <uzunhair@yandex.ru>
2017-04-02 21:35:42 +0300

    no message

---
+++ gulpfile.js
@@ -0,0 +1,274 @@
+"use strict";
+
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    cssnano = require('gulp-cssnano'),
+    pxtorem = require('gulp-pxtorem'),
+    precss = require('precss'),
+    autoprefixer = require('gulp-autoprefixer'),
+    mqpacker = require('css-mqpacker'),
+    //b64i = require('postcss-b64i'),
+    sass = require('gulp-sass'),
+    sassUnicode = require('gulp-sass-unicode'),  // Не допускает ошибок при наличии обратного слеша "\fff"
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'), // Собирает html файлы
+    del = require('del'), // Удаление файлов
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    changed = require('gulp-changed'), // запускают таски только для изменившихся файлов
+    browserSync = require('browser-sync').create();
+
+var duration    = require('gulp-duration');
+
+// https://www.npmjs.com/package/gulp-zip
+
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'dist/',
+        php: 'dist/',
+        js: 'dist/js/',
+        style: 'dist/css/',
+        img: 'dist/img/',
+        fonts: 'dist/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        php: 'src/*.php',
+        js: 'src/js/**/*.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/sass/**/*.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        php: 'src/**/*.php',
+        js: 'src/js/**/*.js',
+        style: 'src/sass/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    browser: {
+        html: 'dist/**/*.html',
+        php: 'dist/**/*.php',
+        js: 'dist/js/**/*.js',
+        style: 'dist/css/**/*.*',
+        img: 'dist/img/**/*.*',
+        fonts: 'dist/fonts/**/*.*'
+    },
+    clean: {
+        html: 'dist/**/*.html',
+        php: 'dist/**/*.php',
+        js: 'dist/js/**/*.js',
+        style: 'dist/css/**/*.*',
+        img: 'dist/img/**/*.*',
+        fonts: 'dist/fonts/**/*.*',
+        all: './dist'
+    }
+};
+
+gulp.task('browser-sync', ['style.min:build'], function() {
+    // browserSync.init({
+    //     proxy: 'gulp.default',
+    //     notify:false
+    // });
+
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+
+});
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(plumber())
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+});
+
+gulp.task('php:build', function () {
+    gulp.src(path.src.php) //Выберем файлы по нужному пути
+        .pipe(plumber())
+        .pipe(gulp.dest(path.build.php)) //Выплюнем их в папку build
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(changed(path.build.js))
+        // .pipe(plumber())
+        // .pipe(rigger())
+        // .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        // .pipe(sourcemaps.write('')) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+});
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) //Выберем наш system.scss
+        .pipe(plumber())
+        .pipe(sourcemaps.init({largeFile: true})) //То же самое что и с js
+        .pipe(sass().on('error', sass.logError)) //Скомпилируем
+        .pipe(sassUnicode())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(pxtorem())
+        .pipe(sourcemaps.write(''))
+        .pipe(gulp.dest(path.build.style)) //И в build
+});
+
+gulp.task('style.min:build', function () {
+    gulp.src(path.src.style) //Выберем наш system.scss
+        .pipe(plumber())
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass().on('error', sass.logError)) //Скомпилируем
+        .pipe(sassUnicode())
+        .pipe(cssnano({zindex: false})) //Сожмем
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(pxtorem())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write(''))
+        .pipe(duration('style.min:build time'))
+        .pipe(gulp.dest(path.build.style)) //И в build
+        .pipe(browserSync.stream())
+});
+
+gulp.task('img:build', function () {
+    var imagemin = require('gulp-imagemin'),
+        imageminGifsicle = require('imagemin-gifsicle'),
+        imageminJpegtran = require('imagemin-jpegtran'),
+        imageminOptipng = require('imagemin-optipng'),
+        imageminSvgo = require('imagemin-svgo');
+
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(changed(path.build.img))
+        .pipe(plumber())
+        .pipe(imagemin([
+            imageminGifsicle({interlaced: true}),
+            imageminJpegtran({progressive: true}),
+            imageminOptipng({optimizationLevel: 5}),
+            imageminSvgo({plugins: [{removeViewBox: true}]})
+        ]))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+});
+
+gulp.task('fonts:build', function () {
+    gulp.src(path.src.fonts) //Выберем файлы по нужному пути
+        .pipe(gulp.dest(path.build.fonts)) //Выплюнем их в папку build
+});
+
+gulp.task('build', [
+    'html:build',
+    'php:build',
+    'js:build',
+    'style:build',
+    'style.min:build',
+    'fonts:build',
+    'img:build'
+]);
+
+gulp.task('watch', function () {
+    gulp.watch(path.watch.php, ['php:build']);
+    gulp.watch(path.watch.html, ['html:build']);
+    gulp.watch(path.watch.style, ['style:build']);
+    gulp.watch(path.watch.style, ['style.min:build']);
+    gulp.watch(path.watch.js, ['js:build']);
+    gulp.watch(path.watch.img, ['img:build']);
+    gulp.watch(path.watch.fonts, ['fonts:build']);
+
+    gulp.watch(path.browser.js).on("change", browserSync.reload);
+    gulp.watch(path.browser.html).on('change', browserSync.reload);
+    gulp.watch(path.browser.php).on('change', browserSync.reload);
+});
+
+/** FTP Configuration **/
+var ftp = require('vinyl-ftp'),
+    gutil = require('gulp-util');
+
+var user = '',
+    password = '',
+    host = '',
+    port = 21,
+    localFilesGlob = [
+        'dist/**/*.html',
+        'dist/js/**/*.js',
+        'dist/css/**/*.css',
+        'dist/img/**/*.*',
+        'dist/fonts/**/*.*'
+    ], remoteFolder = '/';
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {
+    return ftp.create({
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        parallel: 5,
+        log: gutil.log
+    });
+}
+
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ */
+gulp.task('ftp-deploy', function () {
+    var conn = getFtpConnection();
+
+    return gulp.src(localFilesGlob, {base: '.', buffer: false})
+        .pipe(conn.newer(remoteFolder)) // only upload newer files
+        .pipe(conn.dest(remoteFolder));
+});
+
+gulp.task('ftp-deploy-watch', function () {
+
+    var conn = getFtpConnection();
+
+    gulp.watch(localFilesGlob)
+        .on('change', function (event) {
+            console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+
+            return gulp.src([event.path], {base: '.', buffer: false})
+                .pipe(conn.newer(remoteFolder)) // only upload newer files
+                .pipe(conn.dest(remoteFolder));
+        });
+});
+
+gulp.task('ftp', ['build', 'ftp-deploy', 'ftp-deploy-watch', 'watch']);
+/* FTP End **/
+
+// TEST BLOCK/////////////////////
+//////////////////////////////////
+
+gulp.task('zip', function() {
+    var zip = require('gulp-zip');
+    gulp.src([
+        'dist/js/**/*.js',
+        'dist/css/**/*.css'
+    ])
+        .pipe(zip('archives.zip'))
+        .pipe(gulp.dest('dist'))
+});
+
+//////////////////////////////////
+// TEST BLOCK END/////////////////
+
+gulp.task('clean', function () {
+    return del([
+        path.clean.html,
+        path.clean.js,
+        path.clean.style,
+        path.clean.img,
+        path.clean.fonts,
+        path.clean.all
+    ]);
+});
+
+
+gulp.task('default', ['browser-sync', 'build', 'watch']);
---
+++ src/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Title</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/system.css" rel="stylesheet">
+    <link href="/css/theme.css" rel="stylesheet">
+</head>
+<body>
+<header class="header">
+
+</header>
+
+<section class="content">
+    <div class="main">
+        <div class="container">
+            <div class="article">
+                <div class="article-title pt-5">
+                    <h1 class="text-center bg-faded py-4">Help</h1>
+                </div>
+                <div class="small-title">
+                </div>
+                <div class="desc">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+
+</footer>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="../dist/js/bootstrap.min.js"></script>
+</body>
+</html>
