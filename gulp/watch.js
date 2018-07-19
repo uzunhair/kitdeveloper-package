6b0d1c57b90ea05207d1075aea9d56794da3ba71
atid <uzunhair@yandex.ru>
2018-07-19 23:20:53 +0300

    Подключил в Watch минимизацию стилей и спрайт картинок

--- gulp/watch.js
+++ gulp/watch.js
@@ -6,9 +6,9 @@ gulp.task('watch', function () {
     gulp.watch(config.path.watch.pug, ['pug:build']);
     gulp.watch(config.path.watch.styleTheme, ['styleTheme:build', 'styleTypography:build']);
     gulp.watch(config.path.watch.styleVendors, ['styleVendors:build', 'styleTypography:build']);
-    // gulp.watch(config.path.watch.styleTheme, ['styleTheme.min:build']);
-    // gulp.watch(config.path.watch.styleVendors, ['styleVendors.min:build']);
+    gulp.watch(config.path.watch.styleTheme, ['styleTheme.min:build']);
+    gulp.watch(config.path.watch.styleVendors, ['styleVendors.min:build']);
     gulp.watch(config.path.watch.js, ['js:build']);
-    gulp.watch(config.path.watch.img, ['img:build', 'svgSprite:build']);
+    gulp.watch(config.path.watch.img, ['img:build', 'svgSprite:build', 'sprite']);
     gulp.watch(config.path.watch.fonts, ['fonts:build']);
 });
