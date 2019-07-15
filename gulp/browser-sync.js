f05545cde29db82932afdc86a12de11c488746aa
uzunhair <uzunhair@yandex.ru>
2019-07-15 03:24:16 +0300

    Теперь в pug компилируется только измененный файл

--- gulp/browser-sync.js
+++ gulp/browser-sync.js
@@ -12,7 +12,8 @@ gulp.task("browser-sync", () => {
     });
 
     gulp.watch(path.data.watch, gulp.parallel("data"));
-    gulp.watch([path.pug.watch, path.data.dist], gulp.parallel("pug"));
+    gulp.watch(path.data.dist, gulp.parallel("pug"));
+    gulp.watch(path.pug.watch, gulp.parallel("pug:cached"));
     gulp.watch(path.styles.theme.watch, gulp.parallel("styles:theme"));
     gulp.watch(path.styles.vendors.watch, gulp.parallel("styles:vendors"));
     gulp.watch(path.scripts.watch, gulp.parallel("scripts"));
--- gulp/pug.js
+++ gulp/pug.js
@@ -13,6 +13,9 @@ import gulpif from "gulp-if";
 import replace from "gulp-replace";
 import version from 'gulp-version-number';
 import rename from "gulp-rename";
+import cached from "gulp-cached";
+import filter from "gulp-filter";
+import debug from "gulp-debug";
 
 const argv = yargs.argv,
 	production = !!argv.production;
@@ -37,5 +40,32 @@ gulp.task('pug', function () {
 		})))
 		.pipe(duration('pug:build time'))
 		.pipe(gulp.dest(path.pug.dist))
+		.pipe(debug({"title": "Pug files"}))
+		.pipe(browsersync.stream());
+});
+
+gulp.task('pug:cached', function () {
+	return gulp.src(path.pug.src)
+		.pipe(plumber())
+		.pipe(cached('pug'))
+		.pipe(filter(file => `/src/blocks/pages/${file.path}`))
+		.pipe(data(function (file) {
+			return JSON.parse(fs.readFileSync(`${path.data.dist}${path.data.name}`, 'utf8'))
+		}))
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(rename({dirname: '.'}))
+		.pipe(gulpif(production, replace('<link href="css/system.css" rel="stylesheet">', '')))
+		.pipe(gulpif(production, replace('theme.css', 'app.css')))
+		.pipe(gulpif(production, version({
+			'append': {
+				'key': 'v',
+				'to': ['css', 'js'],
+			}
+		})))
+		.pipe(duration('pug:build time'))
+		.pipe(gulp.dest(path.pug.dist))
+		.pipe(debug({"title": "Pug cached files"}))
 		.pipe(browsersync.stream());
 });
